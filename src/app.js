65705982dd3aa53e9b8c720ac2345c5d8f65db30
Jozue06 <jozue06@gmail.com>
2018-07-21 23:42:00 -0700

    runner not running at all

--- src/app.js
+++ src/app.js
@@ -1,101 +1,69 @@
 'use strict';
 
-import nel from 'nel';
-let compileRun = require('compile-run');
+const nel = require('nel');
+// const compileRun = require('compile-run');
 // import fs from 'fs-extra';
 // import auth from '../../auth/middleware.js';
 // import Classes from '../../models/classes.js';
 
-let solution = {};
-let onStdoutArray = [];
-let onStderrArray = [];
+
 
 const run = (obj) => {
+  let solution = {};
+  let onStdoutArray = [];
+  let onStderrArray = [];
   console.log('in runner -->  ', obj);
-  if (obj.language === 'javascript') {
-    var session = new nel.Session();
-    console.log('in session -->');
-    session.execute(obj.code, {
-      onSuccess: (output) => {
-        solution.return = output.mime['text/plain'];
-      },
-      onError: (output) => {
-        solution.error = output.error;
-      },
-      onStdout: (output) => {
-        onStdoutArray.push(output);
-        solution.log = onStdoutArray;
-      },
-      onStderr: (output) => {
-        onStderrArray.push(output);
-        solution['console.error'] = onStderrArray;
-      },
-      afterRun: () => {
-        onStdoutArray = [];
-        let outputResult = {};
-        if (solution.error) {
-          outputResult.error = solution.error;
-          res.send(outputResult);
-        } else if (solution.log && !solution.return) {
-          outputResult.log = solution.log;
-          solution.log = null;
-          let resultArray = outputResult.log;
-          for (let val of resultArray) {
-            resultArray[val] += resultArray[val];
-          }
-          res.send(resultArray);
-        } else if (solution.log && solution.return) {
-          outputResult.log = solution.log;
-          outputResult.return = solution.return;
-          solution.log = null;
-          let resultArray = outputResult.log;
-          for (let val of resultArray) {
-            resultArray[val] += resultArray[val];
-          }
-          let finalResult = resultArray + '\n' + outputResult.return;
-          res.send(finalResult);
-        } else if (!solution.log && solution.return) {
-          outputResult.return = solution.return;
-          res.send(outputResult.return);
-
-        } else if (obj.language === 'python') {
-          let input = null;
-          compileRun.runPython(obj.code, input, function (stdout, stderr, err) {
-            if (!stderr) {
-              // fs.remove(dirPath, err => {
-              //   if (err) return console.error(err);
-              //   console.log('Successfully removed the code dir');
-              // });
-              res.send(stdout);
-            } else {
-              console.log(err);
-              // fs.remove(dirPath, err => {
-              //   if (err) return console.error(err);
-              //   console.log('Successfully removed the code dir');
-              // });
-              res.send(stderr);
-            }
-          });
-        } else if (obj.language === 'java') {
-          let input = null;
-          compileRun.runJava(obj.code, input, function (stdout, stderr, err) {
-            if (!stderr) {
-              // fs.remove(dirPath, err => {
-              //   if (err) return console.error(err);
-              //   console.log('Successfully removed the code dir');
-              // });
-              res.send(stdout);
-            } else {
-              // fs.remove(dirPath, err => {
-              //   if (err) return console.error(err);
-              //   console.log('Successfully removed the code dir');
-              // });
-              console.log(err);
-              res.send(stderr);
-            }
-          });
+  let code = obj.code;
+  var session = new nel.Session();
+  session.execute(code, {
+      
+    onSuccess: (output) => {
+      console.log('in session -->');
+      solution.return = output.mime['text/plain'];
+    },
+      
+    onError: (output) => {
+      console.log('in session --> ');
+      solution.error = output.error;
+    },
+    onStdout: (output) => {
+      console.log('in session --> ');
+      onStdoutArray.push(output);
+      solution.log = onStdoutArray;
+    },
+    onStderr: (output) => {
+      console.log('in session --> ');
+      onStderrArray.push(output);
+      solution['console.error'] = onStderrArray;
+    },
+    afterRun: () => {
+      onStdoutArray = [];
+      let outputResult = {};
+      if (solution.error) {
+        outputResult.error = solution.error;
+      } if (solution.log && !solution.return) {
+        outputResult.log = solution.log;
+        solution.log = null;
+        let resultArray = outputResult.log;
+        for (let val of resultArray) {
+          resultArray[val] += resultArray[val];
         }
-      },
-    });
-  }
+      } if (solution.log && solution.return) {
+        outputResult.log = solution.log;
+        outputResult.return = solution.return;
+        solution.log = null;
+        let resultArray = outputResult.log;
+        for (let val of resultArray) {
+          resultArray[val] += resultArray[val];
+
+        }
+        let finalResult = resultArray + '\n' + outputResult.return;
+        console.log('final results -->  ', finalResult);
+
+      } if (!solution.log && solution.return) {
+        outputResult.return = solution.return;
+      }console.log('in session --> solution2 ', outputResult);
+    },
+  });
+
 };
