ac281e1d25292b5096e2f29627c8fc339abddf94
Jozue06 <jozue06@gmail.com>
2018-07-22 22:03:23 -0700

    borke

---
+++ bad.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const nel = require('nel');
+// const compileRun = require('compile-run');
+// import fs from 'fs-extra';
+// import auth from '../../auth/middleware.js';
+// import Classes from '../../models/classes.js';
+
+module.exports = (obj) => {
+
+
+  let solution = {};
+  let onStdoutArray = [];
+  let onStderrArray = [];
+  console.log('in runner -->  ', obj);
+  let code = obj.code;
+  var session = new nel.Session();
+  session.execute(code, {
+
+    onSuccess: (output) => {
+      console.log('in session -->');
+      solution.return = output.mime['text/plain'];
+    },
+
+    onError: (output) => {
+      console.log('in session --> ');
+      solution.error = output.error;
+    },
+    onStdout: (output) => {
+      console.log('in session --> ');
+      onStdoutArray.push(output);
+      solution.log = onStdoutArray;
+    },
+    onStderr: (output) => {
+      console.log('in session --> ');
+      onStderrArray.push(output);
+      solution['console.error'] = onStderrArray;
+    },
+    afterRun: () => {
+      onStdoutArray = [];
+      let outputResult = {};
+      if (solution.error) {
+        outputResult.error = solution.error;
+      }
+      if (solution.log && !solution.return) {
+        outputResult.log = solution.log;
+        solution.log = null;
+        let resultArray = outputResult.log;
+        for (let val of resultArray) {
+          resultArray[val] += resultArray[val];
+        }
+      }
+      if (solution.log && solution.return) {
+        outputResult.log = solution.log;
+        outputResult.return = solution.return;
+        solution.log = null;
+        let resultArray = outputResult.log;
+        for (let val of resultArray) {
+          resultArray[val] += resultArray[val];
+
+        }
+        let finalResult = resultArray + '\n' + outputResult.return;
+        console.log('final results -->  ', finalResult);
+
+      }
+      if (!solution.log && solution.return) {
+        outputResult.return = solution.return;
+      }
+      console.log('in session --> solution2 ', outputResult);
+    },
+  });
+
+};
